b5c0ad412c1315512a8e37a43be68bedc7e21c41
Dmitry Frolov <mr.rikken@gmail.com>
2018-06-05 21:19:50 +0300

    Label method added to Model.

--- GPSS.NET/GPSS.NET/Model.cs
+++ GPSS.NET/GPSS.NET/Model.cs
@@ -35,5 +35,24 @@ namespace GPSS
         };
 
         object ICloneable.Clone() => Clone();
+
+        /// <summary>
+        /// Labels following Block with given <paramref name="name"/>.
+        /// Block can have many names but every name can address only one Block.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Model with added Label, waiting for next given Block.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="name"/> must not be null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException"><paramref name="name"/> must be unique within the Model Label names.</exception>
+        public Model Label(string name)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+            if (Statements.Labels.ContainsKey(name))
+                throw new ArgumentOutOfRangeException(nameof(name), "Label with given name already exists in the Model.");
+
+            Statements.Labels.Add(name, Statements.Blocks.Count);
+            return this;
+        }
     }
 }
